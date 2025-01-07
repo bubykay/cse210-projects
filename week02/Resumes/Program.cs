b1aea16a92f4687d0ddaa3cedfcfef2066232503
kayode <bubykay@gmail.com>
2025-01-07 18:53:29 +0100

    jobs and resumes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Resume _myResume = new()
+        {
+            _name = "Kayode Adetayo"
+        };
+
+        Job job1 = new()
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Microsoft",
+            _endYear = 2024,
+            _startYear = 2021
+        };
+        _myResume.jobs.Add(job1);
+
+        Job job2 = new()
+        {
+            _jobTitle = "Software Engineer",
+            _company = "LinkedIn",
+            _endYear = 2024,
+            _startYear = 2021
+        };
+        _myResume.jobs.Add(job2);
+
+        _myResume.Display();
     }
 }
